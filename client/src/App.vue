4de5c746513a97c3021e0c40ef87dfe2ec90b600
Demian <devaccdemiann@gmail.com>
2019-11-19 13:17:57 +0100

    Added navbar container and routerview

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Navbar/>
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/layout/Navbar";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    Navbar
+  }
 };
 </script>
 
