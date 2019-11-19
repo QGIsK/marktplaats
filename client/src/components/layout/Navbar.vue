a54171401f8891946a54327199fab13227d8d16b
Demian <devaccdemiann@gmail.com>
2019-11-19 13:19:14 +0100

    Added navbar

---
+++ client/src/components/layout/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand to="/">Marktplaats</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="mx-auto">
+            <b-nav-form>
+              <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+
+          <b-navbar-nav v-if="isLoggedIn" class="ml-auto">
+            <b-nav-item @click="logout()">Logout</b-nav-item>
+            <b-nav-item-dropdown text="User" right>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Settings</b-dropdown-item>
+              <b-dropdown-item href="#">Ads</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+          <b-navbar-nav v-else class="ml-auto">
+            <b-nav-item to="/auth/login">Login</b-nav-item>
+            <b-nav-item to="/auth/register">Register</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+
+  computed: {
+    isLoggedIn: {
+      get() {
+        return this.$store.getters.isLoggedIn;
+      }
+    }
+  }
+};
+</script>
+
