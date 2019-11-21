608c5ab9e7ca0a04238b8af7f132dcf521025fee
Demian <devaccdemiann@gmail.com>
2019-11-21 15:51:03 +0100

    Cleaned up, Added requireAuth and Ad Routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from "vue";
 import Router from "vue-router";
+
 import Home from "@/components/pages/Home";
+
 import Register from "@/components/pages/auth/Register";
 import Login from "@/components/pages/auth/Login";
+
+import MyAds from "@/components/pages/ads/MyAds";
 import ShowAd from "@/components/pages/ads/ShowAd";
+import AddAd from "@/components/pages/ads/AddAd";
+
+import store from "../store/";
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
     routes: [
         {
             path: "/",
@@ -25,9 +32,39 @@ export default new Router({
             component: Login
         },
         {
+            path: "/ad/new",
+            name: "AddAd",
+            component: AddAd,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: "/ad/my",
+            name: "MyAds",
+            component: MyAds,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
             path: "/ad/:id",
             name: "ShowAd",
             component: ShowAd
         }
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isLoggedIn) {
+            next();
+            return;
+        }
+        next("/auth/login");
+    } else {
+        next();
+    }
+});
+
+export default router;
