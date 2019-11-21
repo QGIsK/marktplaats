a5f1816afa7163569be366dc80a735db24f073ce
Demian <devaccdemiann@gmail.com>
2019-11-21 14:53:26 +0100

    Updated

--- client/src/components/pages/ads/ShowAd.vue
+++ client/src/components/pages/ads/ShowAd.vue
@@ -1,9 +1,38 @@
 <template>
-    <div>
-        {{ad}}
+  <div>
+    <b-row class="mt-5 mx-5" v-if="ad">
+      <b-col cols="8">
+        <b-card
+          :title="ad.title"
+          img-src="https://images.unsplash.com/photo-1574321756605-4611fe4ed1a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1360&q=80"
+          img-alt="Image of ad"
+          img-top
+          class="w-75 mx-auto my-4"
+        >
+          <b-card-text>{{ad.description}}</b-card-text>
 
-        <!-- {{allAds}} -->
-    </div> 
+          <b-button @click="redirect('ad', ad.id)" variant="primary">Go to ad</b-button>
+        </b-card>
+      </b-col>
+      <b-col cols="4">
+        <b-card
+          :title="ad.user.name"
+          img-src="https://images.unsplash.com/photo-1574321756605-4611fe4ed1a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1360&q=80"
+          img-alt="Image of ad"
+          img-top
+          class="w-50 my-4"
+        >
+          <b-card-text>
+            Location: {{ad.user.location}}
+            <br>
+            Member since: {{ad.user.created_at || formatDate}}
+          </b-card-text>
+
+          <b-button @click="redirect('user', ad.user.id)" variant="primary">Go to user</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -15,14 +44,19 @@ export default {
       get() {
         return this.allAds.filter(ad => {
           return String(ad.id) === this.$route.params.id;
-        });
+        })[0];
       }
     },
     allAds: {
       get() {
-        return this.$store.getters.ads
+        return this.$store.getters.ads;
       }
     }
+  },
+  methods: {
+    redirect(type, id) {
+      this.$router.push(`/${type}/${id}`);
+    }
   }
-}
+};
 </script>
