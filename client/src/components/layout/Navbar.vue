35895b1e28f7e6f8dd7634524d1e1de9b17720d7
Demian <devaccdemiann@gmail.com>
2019-11-21 15:50:15 +0100

    Cleaned up and added ads

--- client/src/components/layout/Navbar.vue
+++ client/src/components/layout/Navbar.vue
@@ -7,19 +7,16 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-          <!-- <b-navbar-nav class="mx-auto">
-            <b-nav-form>
-              <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-          </b-navbar-nav>-->
-
           <b-navbar-nav v-if="isLoggedIn" class="ml-auto">
-            <b-nav-item @click="logout">Logout</b-nav-item>
+            <b-nav-item-dropdown text="Ads" right>
+              <b-dropdown-item to="/ad/new">New Ad</b-dropdown-item>
+              <b-dropdown-item href="#">My Ads</b-dropdown-item>
+            </b-nav-item-dropdown>
             <b-nav-item-dropdown :text="user.name" right>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item>Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Messages</b-dropdown-item>
               <b-dropdown-item href="#">Settings</b-dropdown-item>
-              <b-dropdown-item href="#">Ads</b-dropdown-item>
+              <b-dropdown-item @click="logout">Logout</b-dropdown-item>
             </b-nav-item-dropdown>
           </b-navbar-nav>
           <b-navbar-nav v-else class="ml-auto">
