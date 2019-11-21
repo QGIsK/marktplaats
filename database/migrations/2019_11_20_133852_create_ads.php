ea7e3719bbb1dcbc021ad95ee7d5b1e7e553085b
Demian <devaccdemiann@gmail.com>
2019-11-21 14:53:45 +0100

    Added diff default image

--- database/migrations/2019_11_20_133852_create_ads.php
+++ database/migrations/2019_11_20_133852_create_ads.php
@@ -20,7 +20,7 @@ class CreateAds extends Migration
             $table->boolean("featured")->default(0);
             $table->string("title");
             $table->longText("description");
-            $table->string("image")->nullable();
+            $table->string("image")->default("https://images.unsplash.com/photo-1574321756605-4611fe4ed1a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1360&q=80");
             $table->softDeletes();
         });
     }
