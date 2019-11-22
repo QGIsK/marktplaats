63de7e20c146aa2edee0a47431a3089de7180d40
Demian <devaccdemiann@gmail.com>
2019-11-22 15:22:13 +0100

    Added axios, and wyiswyg

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,20 @@
 import Vue from "vue";
+import moment from "moment";
+import Axios from "axios";
 import App from "./App.vue";
 import store from "./store";
-import moment from "moment";
 import router from "./router";
+import wysiwyg from "vue-wysiwyg";
 import BootstrapVue from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+
+Vue.use(wysiwyg, {
+    hideModules: { image: true }
+});
 
 Vue.config.productionTip = false;
 
@@ -18,6 +24,20 @@ Vue.filter("formatDate", function(value) {
         return moment(String(value)).format("MM/DD/YYYY");
     }
 });
+
+const token = localStorage.getItem("token");
+
+Vue.prototype.$http = Axios;
+
+Vue.prototype.$http.defaults.baseURL = "http://localhost:8000";
+Vue.prototype.$http.defaults.headers.common["Content-Type"] =
+    "application/json";
+
+if (token) {
+    Vue.prototype.$http.defaults.headers.common[
+        "Authorization"
+    ] = `Bearer ${token}`;
+}
 
 new Vue({
     store,
