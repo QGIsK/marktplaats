03c24e5b1038f72dd3ab7976a337bf4709f682d6
Demian <devaccdemiann@gmail.com>
2019-12-10 16:05:51 +0100

    Added index destroy and store route

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+Use App\Bid;
 use App\Http\Resources\BidResource;
 
 class BidController extends Controller
@@ -12,9 +15,10 @@ class BidController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($ad)
     {
-        //
+        // dd(Bid::where('ad_id', $ad)->with('user')->get());
+        return BidResource::collection(Bid::where('ad_id', $ad)->with('user')->get());
     }
 
     /**
@@ -22,10 +26,26 @@ class BidController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
+     * 
      */
     public function store(Request $request)
     {
-        //
+        if(!$request->amount) {
+            return response()->json(['error' => 'Please provide the amount']);
+        }
+
+        if(!$request->ad_id) {
+            return response()->json(['error' => 'Please provide the ad Id']);
+        }
+        
+        $bid = Bid::create([
+            'amount' => $request->amount,
+            'user_id' => $request->user()->id,
+            'ad_id' => $request->ad_id
+        ]);
+
+        return new BidResource($bid);
+
     }
 
     /**
@@ -34,22 +54,11 @@ class BidController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Bid $bid)
     {
-        //
+        return new BidResource($bid);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
@@ -57,8 +66,13 @@ class BidController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Bid $bid)
     {
-        //
+        if(Auth::user()->id != $bid->user_id ) {
+            return response()->json(['error' => 'Unauthorized'], 403);    
+        }
+        
+        $bid->delete();
+        return response()->json(null, 204);
     }
 }
