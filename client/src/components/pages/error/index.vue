e19f377847aa1be19b73e711d7ab2e2251b6ea59
Demian <devaccdemiann@gmail.com>
2020-02-02 18:32:25 +0100

    Made a 404

---
+++ client/src/components/pages/error/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+
+<script>
+export default {
+  name: "error",
+  mounted() {
+    this.notFound();
+  },
+  methods: {
+    notFound: () => {
+      this.$bvToast.toast(`404 Page not found`, {
+        title: `Error`,
+        variant: "danger",
+        solid: true,
+        autoHideDelay: 5000
+      });
+      this.$router.push("/");
+    }
+  }
+};
+</script>
