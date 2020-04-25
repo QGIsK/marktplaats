e68b4e2c5c74199ddf1df91627209944d2499260
Demian <devaccdemiann@gmail.com>
2020-04-25 22:45:49 +0200

    index store and show

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Message;
+use App\Http\Resources\MessageResource;
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Get all to and from messages to current user
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // save new message and send broadcast + email
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // get all messages by specific user
+    }
+
+}
