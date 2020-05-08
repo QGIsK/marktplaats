c8f4822d3822536896b32c6eff72b22575079935
Demian <devaccdemiann@gmail.com>
2020-05-08 04:16:41 +0200

    integer -> unsigned integer

--- database/migrations/2020_02_28_134844_create_messages_table.php
+++ database/migrations/2020_02_28_134844_create_messages_table.php
@@ -15,8 +15,8 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('to_id')->unsigned();
-            $table->integer('user_id')->unsigned();
+            $table->unsignedInteger('to_id')->unsigned();
+            $table->unsignedInteger('user_id')->unsigned();
             $table->text('message');
             $table->timestamps();
         });
