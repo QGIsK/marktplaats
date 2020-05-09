c7aff2dba932aecd440619aaaae22ab062b907ce
Demian <devaccdemiann@gmail.com>
2020-05-09 04:44:44 +0200

    added chatroom and user

--- database/migrations/2020_02_28_134844_create_messages_table.php
+++ database/migrations/2020_02_28_134844_create_messages_table.php
@@ -15,8 +15,8 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('to_id')->unsigned();
-            $table->unsignedInteger('user_id')->unsigned();
+            $table->unsignedInteger('chat_room')->unsigned();
+            $table->unsignedInteger('user')->unsigned();
             $table->text('message');
             $table->timestamps();
         });
