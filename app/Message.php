c56eb9b0ae2bcc62ffab9ce9152f3d6f7852fbc9
Demian <devaccdemiann@gmail.com>
2020-05-09 04:44:08 +0200

    updated for chatroom

--- app/Message.php
+++ app/Message.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    protected $fillable = ['message', 'user_id', 'to_id'];
+    protected $fillable = ['message', 'chat_room', 'user'];
 
-        
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function chat_room()
+    {
+        return $this->belongsTo(ChatRoom::class);
     }
 }
