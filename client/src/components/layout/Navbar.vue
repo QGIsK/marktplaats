40e7e3c97db18425a9273b042e3ac93274bd22c1
Demian <devaccdemiann@gmail.com>
2020-05-26 16:17:50 +0200

    Changed chat route

--- client/src/components/layout/Navbar.vue
+++ client/src/components/layout/Navbar.vue
@@ -1,63 +1,72 @@
 <template>
-  <div>
-    <b-navbar id="navbar" toggleable="lg" type="dark" variant="dark">
-      <b-container>
-        <b-navbar-brand to="/">Marktplaats</b-navbar-brand>
+    <div>
+        <b-navbar id="navbar" toggleable="lg" type="dark" variant="dark">
+            <b-container>
+                <b-navbar-brand to="/">Marktplaats</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav v-if="isAuthenticated" class="ml-auto">
-            <b-nav-item-dropdown text="Ads" right>
-              <b-dropdown-item to="/ad/new">New Ad</b-dropdown-item>
-              <b-dropdown-item to="/ad/my">My Ads</b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item-dropdown :text="user.name" right>
-              <b-dropdown-item>Profile</b-dropdown-item>
-              <b-dropdown-item to="/messages">Messages</b-dropdown-item>
-              <b-dropdown-item to="/user/settings">Settings</b-dropdown-item>
-              <b-dropdown-item @click="logout">Logout</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-          <b-navbar-nav v-else class="ml-auto">
-            <b-nav-item to="/auth/login">Login</b-nav-item>
-            <b-nav-item to="/auth/register">Register</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-container>
-    </b-navbar>
-  </div>
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav v-if="isAuthenticated" class="ml-auto">
+                        <b-nav-item-dropdown text="Ads" right>
+                            <b-dropdown-item to="/ad/new"
+                                >New Ad</b-dropdown-item
+                            >
+                            <b-dropdown-item to="/ad/my"
+                                >My Ads</b-dropdown-item
+                            >
+                        </b-nav-item-dropdown>
+                        <b-nav-item-dropdown :text="user.name" right>
+                            <b-dropdown-item>Profile</b-dropdown-item>
+                            <b-dropdown-item to="/chats"
+                                >Messages</b-dropdown-item
+                            >
+                            <b-dropdown-item to="/user/settings"
+                                >Settings</b-dropdown-item
+                            >
+                            <b-dropdown-item @click="logout"
+                                >Logout</b-dropdown-item
+                            >
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
+                    <b-navbar-nav v-else class="ml-auto">
+                        <b-nav-item to="/auth/login">Login</b-nav-item>
+                        <b-nav-item to="/auth/register">Register</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Navbar",
+    name: "Navbar",
 
-  computed: {
-    isAuthenticated: {
-      get() {
-        return this.$store.getters.isAuthenticated;
-      }
+    computed: {
+        isAuthenticated: {
+            get() {
+                return this.$store.getters.isAuthenticated;
+            }
+        },
+        user: {
+            get() {
+                return this.$store.getters.user;
+            }
+        }
     },
-    user: {
-      get() {
-        return this.$store.getters.user;
-      }
+    methods: {
+        logout() {
+            this.$store.dispatch("logout").then(() => {
+                this.$router.push("/");
+                this.$bvToast.toast(`Logged out`, {
+                    title: `Success`,
+                    variant: "success",
+                    solid: true,
+                    autoHideDelay: 5000
+                });
+            });
+        }
     }
-  },
-  methods: {
-    logout() {
-      this.$store.dispatch("logout").then(() => {
-        this.$router.push("/");
-        this.$bvToast.toast(`Logged out`, {
-          title: `Success`,
-          variant: "success",
-          solid: true,
-          autoHideDelay: 5000
-        });
-      });
-    }
-  }
 };
 </script>
-
