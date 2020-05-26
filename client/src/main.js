274da941dce406d7995d33fefbc17a461e8eadb9
Demian <devaccdemiann@gmail.com>
2020-05-26 16:17:43 +0200

    Commented out websockets - for later

--- client/src/main.js
+++ client/src/main.js
@@ -57,19 +57,19 @@ if (token) {
 
 if (!token) localStorage.removeItem("user");
 
-import Echo from "laravel-echo";
+// import Echo from "laravel-echo";
 
-window.Pusher = require("pusher-js");
-window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: "oowow",
-    wsHost: window.location.hostname,
-    wsPort: 6001
-});
+// window.Pusher = require("pusher-js");
+// window.Echo = new Echo({
+//     broadcaster: "pusher",
+//     key: "oowow",
+//     wsHost: window.location.hostname,
+//     wsPort: 6001
+// });
 
-window.Echo.channel("DemoChannel").listen("WebsocketDemoEvent", e => {
-    console.log(e);
-});
+// window.Echo.channel("DemoChannel").listen("WebsocketDemoEvent", e => {
+//     console.log(e);
+// });
 
 new Vue({
     store,
