0fb13b76e1923953478d7d14af6bdf777cf41ae7
Demian <devaccdemiann@gmail.com>
2020-05-26 16:17:26 +0200

    Added relation

--- app/ChatRoom.php
+++ app/ChatRoom.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class ChatRoom extends Model
 {
     protected $fillable = ['member1', 'member2'];
+
+    public function member1()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function member2()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function message()
     {
         return $this->hasMany(Message::class);
