5414f85cd8f151aca60fe36b34329e4da91fc230
Demian <devaccdemiann@gmail.com>
2020-06-02 16:15:15 +0200

    Refactored

--- client/src/components/pages/ads/NewAd.vue
+++ client/src/components/pages/ads/NewAd.vue
@@ -1,57 +1,73 @@
 <template>
-  <div>
-    <b-card header="New Ad" class="w-75 mb-5 mt-5 mx-auto">
-      <b-form v-if="show" @submit="onSubmit" @reset="onReset">
-        <b-form-group
-          id="input-group-1"
-          label="Picture of your item"
-          label-for="input-1"
-          description="A picture of the item you're trying to sell."
-        >
-          <vue-dropzone
-            ref="myVueDropzone"
-            id="dropzone"
-            :options="dropzoneOptions"
-            @vdropzone-success="fileSuccess"
-            @vdropzone-error="fileError"
-          ></vue-dropzone>
-        </b-form-group>
+    <div>
+        <b-card header="New Ad" class="w-75 mb-5 mt-5 mx-auto">
+            <b-form v-if="show" @submit="onSubmit" @reset="onReset">
+                <b-form-group
+                    id="input-group-1"
+                    label="Picture of your item"
+                    label-for="input-1"
+                    description="A picture of the item you're trying to sell."
+                >
+                    <vue-dropzone
+                        ref="myVueDropzone"
+                        id="dropzone"
+                        :options="dropzoneOptions"
+                        @vdropzone-success="fileSuccess"
+                        @vdropzone-error="fileError"
+                    ></vue-dropzone>
+                </b-form-group>
 
-        <b-form-group
-          id="input-group-2"
-          label="Ad Title"
-          label-for="input-2"
-          description="What are you trying to sell?"
-        >
-          <b-form-input id="input-2" v-model="title" type="text" required placeholder="Enter title"></b-form-input>
-        </b-form-group>
+                <b-form-group
+                    id="input-group-2"
+                    label="Ad Title"
+                    label-for="input-2"
+                    description="What are you trying to sell?"
+                >
+                    <b-form-input
+                        id="input-2"
+                        v-model="title"
+                        type="text"
+                        required
+                        placeholder="Enter title"
+                    ></b-form-input>
+                </b-form-group>
 
-        <b-form-group
-          id="input-group-3"
-          label="Description"
-          label-for="input-3"
-          description="Describe the item you're trying to sell, Please do include things such as the items Condition And its Age."
-        >
-          <wysiwyg id="body" v-model="description" />
-        </b-form-group>
+                <b-form-group
+                    id="input-group-3"
+                    label="Description"
+                    label-for="input-3"
+                    description="Describe the item you're trying to sell, Please do include things such as the items Condition And its Age."
+                >
+                    <wysiwyg id="body" v-model="description" />
+                </b-form-group>
 
-        <b-form-group label="Categories:" inline>
-          <b-form-checkbox-group v-model="selectedCategories">
-            <span v-for="cat in categories" :key="cat.id">
-              <b-form-checkbox :value="cat.id">{{cat.tag}}</b-form-checkbox>
-            </span>
-          </b-form-checkbox-group>
-        </b-form-group>
+                <b-form-group label="Categories:" inline>
+                    <b-form-checkbox-group v-model="selectedCategories">
+                        <span v-for="cat in categories" :key="cat.id">
+                            <b-form-checkbox :value="cat.id">{{
+                                cat.tag
+                            }}</b-form-checkbox>
+                        </span>
+                    </b-form-checkbox-group>
+                </b-form-group>
 
-        <b-form-group label="Postal code">
-          <b-form-input v-model="postalCode" class="w-25" type="text" required placeholder="1234hz"></b-form-input>
-        </b-form-group>
+                <b-form-group label="Postal code">
+                    <b-form-input
+                        v-model="postalCode"
+                        class="w-25"
+                        type="text"
+                        required
+                        placeholder="1234hz"
+                    ></b-form-input>
+                </b-form-group>
 
-        <b-button type="submit" variant="primary">Create</b-button>
-        <b-button type="reset" variant="danger" class="ml-2">Clear</b-button>
-      </b-form>
-    </b-card>
-  </div>
+                <b-button type="submit" variant="primary">Create</b-button>
+                <b-button type="reset" variant="danger" class="ml-2"
+                    >Clear</b-button
+                >
+            </b-form>
+        </b-card>
+    </div>
 </template>
 
 <script>
@@ -64,138 +80,138 @@ import "vue-wysiwyg/dist/vueWysiwyg.css";
 // import axios from "axios";
 
 export default {
-  name: "NewAd",
-  components: {
-    vueDropzone: vue2Dropzone
-  },
-  data() {
-    return {
-      show: true,
-
-      title: "",
-      description: "",
-      image: "",
-      postalCode: "",
-      selectedCategories: [],
-
-      dropzoneOptions: {
-        url: "http://localhost:8000/api/file/",
-        thumbnailWidth: 150,
-        maxFilesize: 3,
-        maxFiles: 5,
-        headers: { Authorization: `Bearer ${VueCookies.get("token")}` }
-      }
-    };
-  },
-  computed: {
-    ads: {
-      get() {
-        return this.$store.getters.ads;
-      }
+    name: "NewAd",
+    components: {
+        vueDropzone: vue2Dropzone
     },
-    categories: {
-      get() {
-        return this.$store.getters.categories;
-      }
-    }
-  },
-  methods: {
-    fileSuccess(file, res) {
-      this.image.length > 0
-        ? (this.image += `,${res.url}`)
-        : (this.image = res.url);
+    data() {
+        return {
+            show: true,
 
-      console.log(this.image);
+            title: "",
+            description: "",
+            image: "",
+            postalCode: "",
+            selectedCategories: [],
+
+            dropzoneOptions: {
+                url: "http://localhost:8000/api/file/",
+                thumbnailWidth: 150,
+                maxFilesize: 2,
+                maxFiles: 5,
+                headers: { Authorization: `Bearer ${VueCookies.get("token")}` }
+            }
+        };
     },
-    fileError() {
-      this.dropzoneOptions.maxFiles++;
-      this.$bvToast.toast(
-        `An error occured uploading your image, Please try again.`,
-        {
-          title: `Error`,
-          variant: "danger",
-          solid: true,
-          autoHideDelay: 5000
+    computed: {
+        ads: {
+            get() {
+                return this.$store.getters.ads;
+            }
+        },
+        categories: {
+            get() {
+                return this.$store.getters.categories;
+            }
         }
-      );
     },
+    methods: {
+        fileSuccess(file, res) {
+            this.image.length > 0
+                ? (this.image += `,${res.url}`)
+                : (this.image = res.url);
 
-    onSubmit(evt) {
-      evt.preventDefault();
+            console.log(this.image);
+        },
+        fileError() {
+            this.dropzoneOptions.maxFiles++;
+            this.$bvToast.toast(
+                `An error occured uploading your image, Please try again.`,
+                {
+                    title: `Error`,
+                    variant: "danger",
+                    solid: true,
+                    autoHideDelay: 5000
+                }
+            );
+        },
 
-      const data = {
-        title: this.title,
-        image: this.image,
-        description: this.description,
-        postalCode: this.postalCode,
-        categories: this.selectedCategories
-      };
+        onSubmit(evt) {
+            evt.preventDefault();
 
-      this.$store
-        .dispatch("createAd", { ad: data })
-        .then(res => {
-          this.$bvToast.toast(`Ad created`, {
-            title: `Success`,
-            variant: "success",
-            solid: true,
-            autoHideDelay: 5000
-          });
-          let ad = res.data.data;
-          let img = ad.image;
-          img = img.replace('"', "").replace('"', "");
-          img = img.split(",");
-          ad.image = img;
-          this.ads.unshift(ad);
+            const data = {
+                title: this.title,
+                image: this.image,
+                description: this.description,
+                postalCode: this.postalCode,
+                categories: this.selectedCategories
+            };
 
-          this.$router.push(`/ad/${ad.id}`);
-        })
-        .catch(e => {
-          this.$bvToast.toast(`An error occured, Please try again.`, {
-            title: `Error`,
-            variant: "danger",
-            solid: true,
-            autoHideDelay: 5000
-          });
-          console.log(e);
-        });
-      // this.$http
-      // axios({
-      //   url: `/api/ads/`,
-      //   data,
-      //   crossdomain: true,
-      //   method: "POST",
-      //   headers: {
-      //     "Access-Control-Allow-Origin": "*"
-      //   }
-      // })
-      //   .then(res => {
-      //     console.log(res.data);
-      //     // this.$router.push("/");
-      //   })
-      //   .catch(e => {
-      //     console.log(e);
+            this.$store
+                .dispatch("createAd", { ad: data })
+                .then(res => {
+                    this.$bvToast.toast(`Ad created`, {
+                        title: `Success`,
+                        variant: "success",
+                        solid: true,
+                        autoHideDelay: 5000
+                    });
+                    let ad = res.data.data;
+                    let img = ad.image;
+                    img = img.replace('"', "").replace('"', "");
+                    img = img.split(",");
+                    ad.image = img;
+                    this.ads.unshift(ad);
 
-      //     this.$bvToast.toast(`An error occured, Please try again.`, {
-      //       title: `Error`,
-      //       variant: "danger",
-      //       solid: true,
-      //       autoHideDelay: 5000
-      //     });
-      //   });
-    },
-    onReset(evt) {
-      evt.preventDefault();
+                    this.$router.push(`/ad/${ad.id}`);
+                })
+                .catch(e => {
+                    this.$bvToast.toast(`An error occured, Please try again.`, {
+                        title: `Error`,
+                        variant: "danger",
+                        solid: true,
+                        autoHideDelay: 5000
+                    });
+                    console.log(e);
+                });
+            // this.$http
+            // axios({
+            //   url: `/api/ads/`,
+            //   data,
+            //   crossdomain: true,
+            //   method: "POST",
+            //   headers: {
+            //     "Access-Control-Allow-Origin": "*"
+            //   }
+            // })
+            //   .then(res => {
+            //     console.log(res.data);
+            //     // this.$router.push("/");
+            //   })
+            //   .catch(e => {
+            //     console.log(e);
 
-      this.title = "";
-      this.description = "";
-      this.categories = [];
-      this.postalCode = "";
+            //     this.$bvToast.toast(`An error occured, Please try again.`, {
+            //       title: `Error`,
+            //       variant: "danger",
+            //       solid: true,
+            //       autoHideDelay: 5000
+            //     });
+            //   });
+        },
+        onReset(evt) {
+            evt.preventDefault();
 
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
+            this.title = "";
+            this.description = "";
+            this.categories = [];
+            this.postalCode = "";
+
+            this.show = false;
+            this.$nextTick(() => {
+                this.show = true;
+            });
+        }
     }
-  }
 };
 </script>
